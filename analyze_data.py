12eacc026514ceccfa87e61954add4fdc4f73b4a
Amir Dahan <amirdahan@Amirs-MacBook-Pro.local>
2017-05-05 17:05:54 +0300

    change interactions 2,3,5 into binary code (0/1), run using:
    $ python analyze_data.py -h
    usage: analyze_data.py [-h] [--inFile INFILE] [--outFile OUTFILE]
    
    optional arguments:
      -h, --help         show this help message and exit
      --inFile INFILE    an absolute path input file with "user item interaction"
                         pattern
      --outFile OUTFILE  an absolute path output file with dictionary
    
    example:
    $ python analyze_data.py --inFile /Users/amirdahan/RecommenderSystems2017/interactions1m.csv --outFile /Users/amirdahan/RecommenderSystems2017/my_file.npy
    
    Signed-off-by: Amir Dahan <amirdahan@Amirs-MacBook-Pro.local>

--- analyze_data.py
+++ analyze_data.py
@@ -1,32 +1,58 @@
 import matplotlib.pyplot as plt
 import sys
 import argparse
+import numpy
 
 
 #sys.argv[1] if sys.argv[1] is not None else
 
-filename = '/Users/amirdahan/Downloads/data/amir_tmp.txt'
 parser = argparse.ArgumentParser()
-parser.add_argument('--file', help='an absolute path input file with "user item score" pattern')
+parser.add_argument('--inFile', help='an absolute path input file with "user item interaction" pattern')
+parser.add_argument('--outFile', help='an absolute path output file with dictionary')
 args = parser.parse_args()
-print args.file
+print args.inFile
+print args.outFile
 
-f = open(args.file, 'r')
-lines = f.read().splitlines()
+f = open(args.inFile, 'r')
+lines = f.read()
 f.close()
-lines = map(int, lines)
+lines = lines.splitlines()
 
-i=0
+count = 0
 myDict = {}
 for line in lines:
-    i+=1
-    if line in myDict.keys():
-        myDict[line] += 1
-    else:
-        myDict[line] = 1
-    if i%1000 == 0:
-        print i
-print 'done'
+    count+=1
 
-plt.hist(myDict.values())
-plt.show()
+    #skip header
+    if count is 1:
+        continue
+
+    tokens = line.split()
+    key, interaction = tokens[0] + ' ' + tokens[1], int(tokens[2])
+
+    if key in myDict.keys():
+        print "already seen this key"
+    else:
+        myDict[line] = {}
+
+    myDict[line][interaction] = 1
+
+    if count%1000 == 0:
+        print count
+
+print 'woohhoo'
+
+desired_action = {2,3,5}
+count = 0
+for key,value in myDict.items():
+    count += 1
+    if set(desired_action).issubset(value):
+        myDict[key] = 1
+    else:
+        myDict[key] = 0
+
+    if count%1000 == 0:
+        print count
+
+numpy.save(args.outFile, myDict)
+print 'done'
